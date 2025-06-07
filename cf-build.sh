4a296c9f8e0457657dfb1752d16fd82e287f86a5
Luper <lachlannwhitehill@gmail.com>
2025-06-07 21:28:18 +1000

    Change build script to include snap for dart css

--- cf-build.sh
+++ cf-build.sh
@@ -25,6 +25,8 @@ if [ -f "package.json" ]; then
   npm install
 fi
 
+asdf install ruby 3.2.2
+
 echo "Building..."
 
 hugo -b $CF_PAGES_URL --gc --minify --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
