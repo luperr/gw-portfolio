aa7fd5e4f46a487e30227731f1298fa0906cafad
Luper <lachlannwhitehill@gmail.com>
2025-06-07 21:30:45 +1000

    Change build script to include snap for dart css

--- cf-build.sh
+++ cf-build.sh
@@ -3,21 +3,8 @@
 echo "Install Dart Sass Embedded..."
 
 # This should be in the PATH.
-BIN_DIR=/opt/buildhome/.binrc/bin
+asdf install ruby 3.2.2
 
-DARTSASS_VERSION=1.63.4
-
-mkdir -p $BIN_DIR
-
-curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
-
-tar -xvf dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz;
-
-mv dart-sass $BIN_DIR
-
-rm -rf dart-sass*;
-
-sass --embedded --version
 
 echo "Installing node dependacies"
 
