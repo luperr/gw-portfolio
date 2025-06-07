36d3c223d3ebab5aa1bc66494e229a13916cf8fd
Luper <lachlannwhitehill@gmail.com>
2025-06-07 20:32:57 +1000

    update build script

--- cf-build.sh
+++ cf-build.sh
@@ -4,6 +4,4 @@ if [ -f "package.json" ]; then
   npm install
 fi
 
-hugo -b $CF_PAGES_URL --gc --minify --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
-
-echo $CF_PAGES_URL
+hugo -b $CF_PAGES_URL --environment production --gc --minify --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
