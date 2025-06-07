c6fcd164192c3df285ce34473b068d1ec002a8d1
Luper <lachlannwhitehill@gmail.com>
2025-06-07 21:42:01 +1000

    Change build script to include snap for dart css

--- cf-build.sh
+++ cf-build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-echo "Install Dart Sass Embedded..."
-
-# This should be in the PATH.
-asdf install ruby 3.2.2
-
-
 echo "Installing node dependacies"
 
 if [ -f "package.json" ]; then
   npm install
 fi
-
-asdf install ruby 3.2.2
 
 echo "Building..."
 
